cb9d76c8b6692a930d1879f180627d2be927c58e
Lenardar <hangzhouliuhaoran@163.com>
2024-05-05 18:28:54 +0800

    实现红黑树

--- module/Tree.py
+++ module/Tree.py
@@ -356,3 +356,229 @@ class AVLTree(BinarySearchTree):
                 else:
                     currentNode.replaceNodeData(currentNode.rightChild.key, currentNode.rightChild.value, currentNode.rightChild.leftChild, currentNode.rightChild.rightChild)
         self.updateBalance(currentNode)
+
+
+# 实现红黑树
+class RedBlackTree():
+    def __init__(self):
+        self.root = None
+        self.size = 0
+        
+    def length(self):
+        return self.size
+    
+    def __len__(self):
+        return self.size
+    
+    def __iter__(self):
+        return self.root.__iter__()
+    
+    def put(self, key, val):
+        if self.root:
+            self._put(key, val, self.root)
+        else:
+            self.root = TreeNode(key, val)
+            self.size += 1
+            self.root.color = 'black'
+            
+    def _put(self, key, val, currentNode):
+        if key < currentNode.key:
+            if currentNode.hasLeftChild():
+                self._put(key, val, currentNode.leftChild)
+            else:
+                currentNode.leftChild = TreeNode(key, val, parent=currentNode)
+                self.size += 1
+                self.fixRedViolation(currentNode.leftChild)
+        else:
+            if currentNode.hasRightChild():
+                self._put(key, val, currentNode.rightChild)
+            else:
+                currentNode.rightChild = TreeNode(key, val, parent=currentNode)
+                self.size += 1
+                self.fixRedViolation(currentNode.rightChild)
+                
+    def fixRedViolation(self, node):
+        while node != self.root and node.parent.color == 'red':
+            if node.parent == node.parent.parent.leftChild:
+                uncle = node.parent.parent.rightChild
+                if uncle.color == 'red':
+                    node.parent.color = 'black'
+                    uncle.color = 'black'
+                    node.parent.parent.color = 'red'
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.rightChild:
+                        node = node.parent
+                        self.rotateLeft(node)
+                    node.parent.color = 'black'
+                    node.parent.parent.color = 'red'
+                    self.rotateRight(node.parent.parent)
+            else:
+                uncle = node.parent.parent.leftChild
+                if uncle.color == 'red':
+                    node.parent.color = 'black'
+                    uncle.color = 'black'
+                    node.parent.parent.color = 'red'
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.leftChild:
+                        node = node.parent
+                        self.rotateRight(node)
+                    node.parent.color = 'black'
+                    node.parent.parent.color = 'red'
+                    self.rotateLeft(node.parent.parent)
+        self.root.color = 'black'
+        
+    def rotateLeft(self, rotRoot):
+        newRoot = rotRoot.rightChild
+        rotRoot.rightChild = newRoot.leftChild
+        if newRoot.leftChild != None:
+            newRoot.leftChild.parent = rotRoot
+        newRoot.parent = rotRoot.parent
+        if rotRoot.isRoot():
+            self.root = newRoot
+        else:
+            if rotRoot.isLeftChild():
+                rotRoot.parent.leftChild = newRoot
+            else:
+                rotRoot.parent.rightChild = newRoot
+        newRoot.leftChild = rotRoot
+        rotRoot.parent = newRoot
+        
+    def rotateRight(self, rotRoot):
+        newRoot = rotRoot.leftChild
+        rotRoot.leftChild = newRoot.rightChild
+        if newRoot.rightChild != None:
+            newRoot.rightChild.parent = rotRoot
+        newRoot.parent = rotRoot.parent
+        if rotRoot.isRoot():
+            self.root = newRoot
+        else:
+            if rotRoot.isRightChild():
+                rotRoot.parent.rightChild = newRoot
+            else:
+                rotRoot.parent.leftChild = newRoot
+        newRoot.rightChild = rotRoot
+        rotRoot.parent = newRoot
+        
+    def __setitem__(self, k, v):
+        self.put(k, v)
+
+    def get(self, key):
+        if self.root:
+            res = self._get(key, self.root)
+            if res:
+                return res.value
+            else:
+                return None
+        else:
+            return None
+        
+    def _get(self, key, currentNode):
+        if not currentNode:
+            return None
+        if key == currentNode.key:
+            return currentNode
+        if key < currentNode.key:
+            return self._get(key, currentNode.leftChild)
+        else:
+            return self._get(key, currentNode.rightChild)
+        
+    def __getitem__(self, key):
+        return self.get(key)
+    
+    def __contains__(self, key):
+        if self._get(key, self.root):
+            return True
+        else:
+            return False
+        
+    def delete(self, key):
+        if self.size > 1:
+            currentNode = self._get(key, self.root)
+            if currentNode:
+                self.remove(currentNode)
+                self.size -= 1
+            else:
+                raise KeyError('Error, key not in tree')
+        elif self.size == 1 and self.root.key == key:
+            self.root = None
+            self.size -= 1
+        else:
+            raise KeyError('Error, key not in tree')
+        
+    def __delitem__(self, key):
+        self.delete(key)
+        
+    def remove(self, currentNode):
+        if currentNode.isLeaf():
+            if currentNode == currentNode.parent.leftChild:
+                currentNode.parent.leftChild = None
+            else:
+                currentNode.parent.rightChild = None
+        elif currentNode.hasBothChildren():
+            succ = currentNode.findSuccessor()
+            succ.spliceOut()
+            currentNode.key = succ.key
+            currentNode.value = succ.value
+        else:
+            if currentNode.hasLeftChild():
+                if currentNode.isLeftChild():
+                    currentNode.leftChild.parent = currentNode.parent
+                    currentNode.parent.leftChild = currentNode.leftChild
+                elif currentNode.isRightChild():
+                    currentNode.leftChild.parent = currentNode.parent
+                    currentNode.parent.rightChild = currentNode.leftChild
+                else:
+                    currentNode.replaceNodeData(currentNode.leftChild.key, currentNode.leftChild.value, currentNode.leftChild.leftChild, currentNode.leftChild.rightChild)
+            else:
+                if currentNode.isLeftChild():
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.leftChild = currentNode.rightChild
+                elif currentNode.isRightChild():
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.rightChild = currentNode.rightChild
+                else:
+                    currentNode.replaceNodeData(currentNode.rightChild.key, currentNode.rightChild.value, currentNode.rightChild.leftChild, currentNode.rightChild.rightChild)
+        self.fixRedViolation(currentNode)
+        
+    def findSuccessor(self):
+        succ = None
+        if self.hasRightChild():
+            succ = self.rightChild.findMin()
+        else:
+            if self.parent:
+                if self.isLeftChild():
+                    succ = self.parent
+                else:
+                    self.parent.rightChild = None
+                    succ = self.parent.findSuccessor()
+                    self.parent.rightChild = self
+        return succ
+    
+    def findMin(self):
+        current = self
+        while current.hasLeftChild():
+            current = current.leftChild
+        return current
+    
+    def spliceOut(self):
+        if self.isLeaf():
+            if self.isLeftChild():
+                self.parent.leftChild = None
+            else:
+                self.parent.rightChild = None
+        elif self.hasAnyChildren():
+            if self.hasLeftChild():
+                if self.isLeftChild():
+                    self.parent.leftChild = self.leftChild
+                else:
+                    self.parent.rightChild = self.leftChild
+                self.leftChild.parent = self.parent
+            else:
+                if self.isLeftChild():
+                    self.parent.leftChild = self.rightChild
+                else:
+                    self.parent.rightChild = self.rightChild
+                self.rightChild.parent = self.parent
+                
