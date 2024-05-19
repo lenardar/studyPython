4b456ddc0068208b48949a25ec1abbe0d77e965a
Lenardar <hangzhouliuhaoran@163.com>
2024-05-19 19:54:26 +0800

    实现优先级队列

--- module/Queue.py
+++ module/Queue.py
@@ -14,3 +14,23 @@ class Queue:
 
     def size(self):
         return len(self.items)
+    
+
+
+# 实现优先级队列
+class PriorityQueue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item, priority):  # 入队
+        self.items.append((item, priority))
+        self.items.sort(key=lambda x: x[1], reverse=True)
+
+    def dequeue(self):  # 出队
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
